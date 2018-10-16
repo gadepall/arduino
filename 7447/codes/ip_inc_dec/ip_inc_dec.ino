69d99d9c944fa3dd96a28cc632dfc87b8df4169c
gadepall <gadepall@gmail.com>
2018-10-16 15:20:23 +0530

    Add existing files

--- 7447/codes/ip_inc_dec/ip_inc_dec.ino
+++ 7447/codes/ip_inc_dec/ip_inc_dec.ino
@@ -1,6 +1,5 @@
 //Declaring all variables as integers
-int Z=0,Y=0,X=0,W=1;
-int D,C,B,A;
+int Z,Y,X,W;
 
 //Code released under GNU GPL.  Free to use for anything.
 void disp_7447(int D, int C, int B, int A)
@@ -17,17 +16,20 @@ void setup() {
     pinMode(3, OUTPUT);
     pinMode(4, OUTPUT);
     pinMode(5, OUTPUT);
+    pinMode(6, INPUT);  
+    pinMode(7, INPUT);
+    pinMode(8, INPUT);
+    pinMode(9, INPUT);
+    
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  A=0;
-  B=(W&&!X&&!Y&&!Z) || (!W&&X&&!Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  C=(W&&X&&!Y&&!Z) || (!W&&!X&&Y&&!Z) || (W&&!X&&Y&&!Z) || (!W&&X&&Y&&!Z);
-  D = (W&&X&&Y&&!Z)||(!W&&!X&&!Y&&Z);
   
-disp_7447(D,C,B,A);  
+W = digitalRead(6);//LSB  
+X = digitalRead(7);  
+Y = digitalRead(8);  
+Z = digitalRead(9);//MSB  
+  
+disp_7447(Z,Y,X,W);  
 }
-//&& is the AND operation
-// || is the OR operation
-// ! is the NOT operation
