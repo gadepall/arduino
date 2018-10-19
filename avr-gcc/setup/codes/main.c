a230e01c02a8cd5f0d42ab5fa1ef0d09c84a14fe
gadepall <gadepall@gmail.com>
2018-10-19 19:58:10 +0530

    Add existing files

--- avr-gcc/setup/codes/main.c
+++ avr-gcc/setup/codes/main.c
@@ -11,11 +11,12 @@ int main (void)
  //set PB5, pin 13 of arduino as output
   DDRB    |= ((1 << DDB5));
   while (1) {
-//turn led on    
-//    PORTB = ((0 <<  PB5));
-
-//turn led off
+//turn led off    
+    PORTB = ((0 <<  PB5));
+	_delay_ms(500);
+//turn led on
     PORTB = ((1 <<  PB5));
+    _delay_ms(500);
   }
 
   /* . */
